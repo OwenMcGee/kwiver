05caf57ec3624c0a8ca166b4a7adfbe56bc203ca
OwenMcGee <owenmcgee69@gmail.com>
2022-03-25 16:37:38 -0400

    Added outline of tests for untested functions

---
+++ vital/tests/test_feature_track_set.cxx
@@ -0,0 +1,53 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+/// \file
+/// \brief test core feature_track_set class
+
+#include <vital/types/feature_track_set.h>
+
+#include <gtest/gtest.h>
+
+// ----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest( &argc, argv );
+  return RUN_ALL_TESTS();
+}
+
+// ----------------------------------------------------------------------------
+TEST(feature_track_set, clone)
+{
+
+}
+
+// ----------------------------------------------------------------------------
+TEST(feature_track_set, last_frame_features)
+{
+
+}
+
+// ----------------------------------------------------------------------------
+TEST(feature_track_set, last_frame_descriptors)
+{
+
+}
+
+// ----------------------------------------------------------------------------
+TEST(feature_track_set, frame_feaure_info)
+{
+
+}
+
+// ----------------------------------------------------------------------------
+TEST(feature_track_set, all_feature_frame_data)
+{
+
+}
+
+// ----------------------------------------------------------------------------
+TEST(feature_track_set, feature_frame_data)
+{
+
+}
